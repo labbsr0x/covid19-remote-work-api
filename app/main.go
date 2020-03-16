353cd3a88825e47e501a1896adcd6bfbb0041692
tiagostutz <tiagostutz@gmail.com>
2020-03-16 09:22:37 -0300

    added flags to the URLs of the external API services used by enrollment process

--- app/main.go
+++ app/main.go
@@ -16,8 +16,22 @@ type User struct {
 	KitURL string `json:"kitDownloadURL,empty"`
 }
 
+// storageAPIURL is the URL of a storage provider used to persist the certificates and other information
 var storageAPIURL string
+
+// certificateIssuerAPIURL is the URL of the service that issues a user certificate
+var certificateIssuerAPIURL string
+
+// userRolesAPIURL is the URL of the service used to fetch users roles
+var userRolesAPIURL string
+
+// conductorAPIURL is the URL of the service that triggers and manages a conductor used to build and store the virtual machines
+var conductorAPIURL string
+
+// baseURL is this service base URL. Used to build output and return URLs
 var baseURL string
+
+// fileServerVMUsersPath is the path in the file server to persist
 var fileServerVMUsersPath string
 
 func main() {
@@ -27,10 +41,17 @@ func main() {
 	logrus.Infof("Starting Remote Kit Builder API")
 	logrus.SetLevel(logrus.DebugLevel)
 	flag.StringVar(&storageAPIURL, "storageAPIURL", "http://localhost:3000", "Simple File storage URL to store Virtual Machine images and configurations")
+	flag.StringVar(&certificateIssuerAPIURL, "certificateIssuerAPIURL", "http://localhost:3005", "Certificate issuer service URL that will issue VPN certificates for users")
+	flag.StringVar(&userRolesAPIURL, "userRolesAPIURL", "http://localhost:3006", "URL of the service used to fetch users roles")
+	flag.StringVar(&conductorAPIURL, "conductorAPIURL", "http://localhost:3007", "URL of the service that triggers and manages a conductor used to build and store the virtual machines")
 	flag.StringVar(&baseURL, "baseURL", "http://localhost:8000", "Base URL of this service, used to print responses")
 	flag.Parse()
 
 	logrus.Infof("storageAPIURL=%s", storageAPIURL)
+	logrus.Infof("certificateIssuerAPIURL=%s", certificateIssuerAPIURL)
+	logrus.Infof("userRolesAPIURL=%s", userRolesAPIURL)
+	logrus.Infof("conductorAPIURL=%s", conductorAPIURL)
+	logrus.Infof("baseURL=%s", baseURL)
 
 	router := mux.NewRouter()
 	router.Handle("/metrics", promhttp.Handler()).Methods("GET")
--- startup.sh
+++ startup.sh
@@ -2,4 +2,7 @@
 
 ./covid19-remote-work-api \
     --storageAPIURL=$STORAGE_API_URL \
+    --certificateIssuerAPIURL=$CERTIFICATE_ISSUER_API_URL \
+    --userRolesAPIURL=$USER_ROLED_API_URL \
+    --conductorAPIURL=$CONDUCTOR_API_URL \
     --baseURL=$BASE_URL
